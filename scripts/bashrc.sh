6699e6a120c9dec108269305dc1fbb7427cb95ec
Cosmin Ciocan <ciocan.cosmin98@gmail.com>
2022-12-17 19:40:57 +0200

    Add comment to bashrc script file

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,2 +1,4 @@
+# these lines are added to ~/.bashrc in the container so every time you connect
+# you are in the right directory with the virtual environment active
 source /opt/venv/bin/activate
 cd /storage
